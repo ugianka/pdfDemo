50d2a53f1129ba7722518d9ee90fdc9ab82f0642
Giancarlo.Sanna <giancarlo.sanna@it.ibm.com>
2017-08-30 09:17:26 +0200

    mm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { DomSanitizer,SafeHtml,SafeUrl } from '@angular/platform-browser';
+
+declare var jsPDF;
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+  doc: any;
+  constructor(private sanitizer: DomSanitizer) {
+    
+  }
+  
+  ngOnInit() {
+    this.doc = new jsPDF();
+    this.doc.text(20, 20, 'Hello world!');
+    this.doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+    this.doc.addPage();
+    this.doc.text(20, 20, 'Do you like that?');
+    // Save the PDF
+  }
+  printPdf() {
+    
+   
+    this.doc.save('Test.pdf');
+  }
+
+  optputPDF() {
+    this.doc.output('datauri');
+  }
+
+
+  getIframeContent():SafeUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl( this.doc.output('datauristring'));
+  }
+
+
+
 }
